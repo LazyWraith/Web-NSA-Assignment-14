6dbe3752af6728e9311cab9ed0b18440f8ac5543
Weng Hong Lee <97719857+AlphaTheWoof@users.noreply.github.com>
2023-06-15 13:24:22 +0800

    Initial merge from PoC to production server (code may not work)

---
+++ site2/verifytoken.php
@@ -0,0 +1,43 @@
+<?php
+
+//Establishing connection
+$connection = mysqli_connect('account.nsagroup14.com', 'user', 'PAFBAWUayq9U7oWz', 'nsa');
+
+if (!$connection)
+{
+    echo 'Connection error: ' . mysqli_connect_error();
+}
+
+$token = mysqli_real_escape_string($connection, $_POST['token']);
+
+$sql = "SELECT token FROM login WHERE token = '$token'";
+$result = mysqli_query($connection, $sql);
+$tokeninfos = mysqli_fetch_assoc($result);
+
+if(!empty($tokeninfos))
+{
+    echo "true";
+}
+else echo "";
+
+// $check = 0;
+
+// for ($i = 0; $i < count($tokeninfos); $i++)
+// {
+//     if ($token == $tokeninfos[$i]["token"])
+//     {
+//         $check++;
+//         break;
+//     }
+// }
+
+// if ($check != 0)
+// {
+//     echo 'true';
+// }
+// else
+// {
+//     echo '';
+// }
+
+?>
